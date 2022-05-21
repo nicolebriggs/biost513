6068eb27641d34ff2c7da724f6efbb165058d92c
caitlindrover <104599613+caitlindrover@users.noreply.github.com>
2022-05-21 10:57:40 -0700

    Delete Data Analysis Project.R

--- Data Analysis Project.R
+++
@@ -1,9 +0,0 @@
-# Biost 513 Data Analysis Project 
-# 5/21/22
-
-#rm(list = ls())
-setwd("~/Desktop/biost513/data")
-source("~/Desktop/biost513/code/compute_measures.R")
-
-data <- read.csv("~/Desktop/biost513/data/dataproject2022.csv")
-
